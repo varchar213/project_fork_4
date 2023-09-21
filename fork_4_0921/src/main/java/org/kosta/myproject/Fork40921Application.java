6aa8e377c96024b8d08353aec74ffe8d198b7c0a
varchar213 <jmcs7eg1@gmail.com>
2023-09-21 17:53:00 +0900

    git-이클립스  스프링부트 기본 먼저 연동

---
+++ fork_4_0921/src/main/java/org/kosta/myproject/Fork40921Application.java
@@ -0,0 +1,13 @@
+package org.kosta.myproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Fork40921Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Fork40921Application.class, args);
+	}
+
+}
